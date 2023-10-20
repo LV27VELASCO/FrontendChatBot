4707044e259d72add5c2778cf5e967e5b0524c79
Luis Velasco <luiselsw27@gmail.com>
2023-10-20 18:06:56 -0500

    browse

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -9,8 +9,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
